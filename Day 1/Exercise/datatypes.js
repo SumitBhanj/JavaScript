87094d6507de110f9fca7fa7579d889a1437a193
Sumit Bhanj <84090392+SumitBhanj@users.noreply.github.com>
2025-07-23 21:12:13 +0530

    Add files via upload

---
+++ Day 1/Exercise/datatypes.js
@@ -0,0 +1,28 @@
+let name = "sumit", isTrue = true, valid, isNull = null
+
+console.log(typeof(name))
+console.log(typeof(isTrue))
+console.log(typeof(valid))
+console.log(typeof(isNull))
+
+// Declare four variables without assigning values
+let shazam, batman, superman, flash
+
+// Declare four variables with assigned values
+let ironman = "tony", spiderman = "peter", hulk = "bruce", captain = "steve"
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let fName = "Tony" 
+let lName = "Stark"
+let status = "Married"
+let country = "United States"
+let age = 53
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let fName2 = "Tony", lName2 = "Stark",status2 = "Married",country2 = "United States",age2 = 53
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 20, yourAge = 30
+console.log("I am ",myAge," years old.");
+console.log("You are ",yourAge," years old");
+
